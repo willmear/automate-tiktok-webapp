d76e4eb362c8d214916c4f019656714f83ea5436
Will Mear <wxm158@student.bham.ac.uk>
2024-06-20 10:21:58 +0100

    create generate_videos

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('generate_video/', include('generate_video.urls')),
     path('admin/', admin.site.urls),
 ]
